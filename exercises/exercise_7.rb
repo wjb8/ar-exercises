1ab1b3e9b035ad53569bae952858996bb6c2330c
James Brown <brownj95@gmail.com>
2021-05-25 00:47:48 +0000

    Complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,26 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
+### Exercise 7: Validations for both models
+
+# 1. Add validations to two models to enforce the following business rules:
+#   * Employees must always have a first name present
+#   * Employees must always have a last name present
+#   * Employees have a hourly_rate that is a number (integer) between 40 and 200
+#   * Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+#   * Stores must always have a name that is a minimum of 3 characters
+#   * Stores have an annual_revenue that is a number (integer) that must be 0 or more
+#   * BONUS: Stores must carry at least one of the men's or women's apparel 
+#   (hint: use a [custom validation method](http://guides.rubyonrails.org/active_record_validations.html#custom-methods) - 
+#   **don't** use a `Validator` class)
+# 2. Ask the user for a store name (store it in a variable)
+# 3. Attempt to create a store with the inputted name but leave out the other fields 
+#    (annual_revenue, mens_apparel, and womens_apparel)
+# 4. Display the error messages provided back from ActiveRecord to the user (one on each line) after you 
+#    attempt to save/create the record
 # Your code goes here ...
+
+print "Enter store name: "
+store_name = gets.chomp
+new_store = Store.create(name: store_name)
+puts new_store.errors.full_messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+  # validate :mens_or_womens
+
+  # def mens_or_womens
+  #   if !mens_apparel && !womens_apparel
+  #     errors.add("Store must sell either men's or women's apparel")
+  #   end
+  # end
 
 end
